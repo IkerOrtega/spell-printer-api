8bc5ff9475db8dae40e34b90a64b6c3f5d4730df
IkerOrtega <32632371+IkerOrtega@users.noreply.github.com>
2025-11-20 12:44:40 +0100

    update Dockerfile

--- src/main/java/com/ikerortega/spell_printer/util/Constants.java
+++ src/main/java/com/ikerortega/spell_printer/util/Constants.java
@@ -2,5 +2,5 @@ package com.ikerortega.spell_printer.util;
 
 public class Constants {
 
-    public static final String SPELLS_CSV_PATH =  "data/csv/spells.csv";
+    public static final String SPELLS_CSV_PATH =  "/data/csv/spells.csv";
 }
