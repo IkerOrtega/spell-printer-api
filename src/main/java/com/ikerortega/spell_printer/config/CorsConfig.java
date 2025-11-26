f8a3ad1c435b63d67cdbfb7637867bbb1ed8f3db
bgd <bberghie@gmail.com>
2025-11-26 21:45:42 +0100

    Allow only GET.

--- src/main/java/com/ikerortega/spell_printer/config/CorsConfig.java
+++ src/main/java/com/ikerortega/spell_printer/config/CorsConfig.java
@@ -34,7 +34,7 @@ public class CorsConfig {
         corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173", FRONT_CI_URL));
         corsConfiguration.setAllowedHeaders(List.of(ORIGIN, ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_TYPE, ACCEPT, AUTHORIZATION, X_REQUESTED_WITH, ACCESS_CONTROL_REQUEST_METHOD, ACCESS_CONTROL_REQUEST_HEADERS, ACCESS_CONTROL_ALLOW_CREDENTIALS));
         corsConfiguration.setExposedHeaders(List.of(ORIGIN, ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_TYPE, ACCEPT, AUTHORIZATION, X_REQUESTED_WITH, ACCESS_CONTROL_REQUEST_METHOD, ACCESS_CONTROL_REQUEST_HEADERS, ACCESS_CONTROL_ALLOW_CREDENTIALS));
-        corsConfiguration.setAllowedMethods(List.of(GET.name(), POST.name(), PUT.name(), PATCH.name(), DELETE.name(), OPTIONS.name()));
+        corsConfiguration.setAllowedMethods(List.of(GET.name()));
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsFilter(urlBasedCorsConfigurationSource);
     }
